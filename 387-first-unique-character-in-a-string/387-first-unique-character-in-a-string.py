3a2394a2d2c1910e81223ff47c1f79d56c19c6ea
Alagar Samy S <91303910+AlagarSamy2@users.noreply.github.com>
2022-05-07 09:27:34 +0530

    Time: 174 ms (40.73%), Space: 14.1 MB (96.59%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -3,11 +3,9 @@ class Solution:
         s1={}
         for i in range(0,len(s)):
             if s[i] in s1:
-                # print(i)
                 s1[s[i]]+=1
             else:
                 s1[s[i]]=1
-        print(s1)
         for j in s1.keys():
             if s1[j]==1:
                 print(s1[j])
