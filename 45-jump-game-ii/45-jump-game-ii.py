26784fe129a227860b10139a3655db0b385adcfd
Alagar Samy S <91303910+AlagarSamy2@users.noreply.github.com>
2022-07-26 12:09:18 +0530

    Time: 1938 ms (29.88%), Space: 15.3 MB (12.75%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        ptr = len(nums)-1
+        count=0
+        while ptr!=0:
+            for i in range(0,ptr):
+                if nums[i]>= ptr-i:
+                    ptr=i
+                    count+=1
+                    print(count)
+                    break
+            
+        return count
