96ffd76238d91afdc2e66408db0e8964e795168b
Alagar Samy S <91303910+AlagarSamy2@users.noreply.github.com>
2022-08-03 09:49:39 +0530

    Time: 129 ms (98.88%), Space: 16.7 MB (84.56%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,9 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        d={}
+        ans=0
         for i in range(len(nums)):
-            d[nums[i]]=d.get(nums[i], 0)+1
-        for i in range(len(nums)):
-            if d[nums[i]]==1:
-                return nums[i]
+            ans=ans^nums[i]
+        return ans
         
