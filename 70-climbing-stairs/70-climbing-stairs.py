0a3bc38a5714b0f3e4471db40aa7251348b96353
Alagar Samy S <91303910+AlagarSamy2@users.noreply.github.com>
2022-08-03 11:51:00 +0530

    Time: 36 ms (81.58%), Space: 14 MB (11.90%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,13 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one,two=1,1
-        temp=0
-        total=0
-        n=n-2
-        while n>=0:
-            total=one+two
-            one,two=total,one
-            n-=1
-        return one 
-    
+        i,j=1,1
+        while n>1:
+            temp=j
+            j=j+i
+            i=temp
+            n=n-1
+        return j
         
