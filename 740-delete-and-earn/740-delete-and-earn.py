3958612054ce3031b9cc25fac13d4bcee1dde563
Alagar Samy S <91303910+AlagarSamy2@users.noreply.github.com>
2022-08-04 11:57:40 +0530

    Time: 148 ms (46.30%), Space: 16.6 MB (29.95%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,20 +1,23 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         d={}
+        nums.sort()
         for i in range(len(nums)):
             d[nums[i]]=d.get(nums[i],0)+1
-        print(d)
-        nums=sorted(list(set(nums)))
-        earn1,earn2=0,0
-        for i in range(len(nums)):
+        nums=list(set(nums))
+        dp=nums
+        cur_value=0
+        earn1,earn2=0,nums[0]*d[nums[0]]
+        for i in range(1,len(nums)):
             cur_value=nums[i]*d[nums[i]]
-            if i>0 and nums[i]==nums[i-1]+1:
+            if nums[i]==nums[i-1]+1:
                 temp=earn2
-                earn2=max(cur_value+earn1,earn2)
+                earn2=max(earn2,cur_value+earn1)
                 earn1=temp
             else:
                 temp=earn2
                 earn2=cur_value+earn2
                 earn1=temp
         return earn2
+                
                 
