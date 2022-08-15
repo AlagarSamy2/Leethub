fb91248a0734031cddfd6a37370f8a65f244f703
Alagar Samy S <91303910+AlagarSamy2@users.noreply.github.com>
2022-08-15 09:23:37 +0530

    Time: 178 ms (83.77%), Space: 13.9 MB (84.39%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.py
+++ 264-ugly-number-ii/264-ugly-number-ii.py
@@ -4,11 +4,14 @@ class Solution:
         dp.append(1)
         p2,p3,p5=0,0,0
         for i in range(1,n):
-            dp.append(min(dp[p2]*2,dp[p3]*3,dp[p5]*5))
-            if dp[i]==dp[p2]*2:
+            mul2=dp[p2]*2
+            mul3=dp[p3]*3
+            mul5=dp[p5]*5
+            dp.append(min(mul2,mul3,mul5))
+            if dp[i]==mul2:
                 p2+=1
-            if dp[i]==dp[p3]*3:
+            if dp[i]==mul3:
                 p3+=1
-            if dp[i]==dp[p5]*5:
+            if dp[i]==mul5:
                 p5+=1
         return dp[n-1]
