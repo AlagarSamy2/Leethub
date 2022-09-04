583816997add9536876e7af9ac279a0221c56622
Alagar Samy S <91303910+AlagarSamy2@users.noreply.github.com>
2022-09-04 17:10:15 +0530

    Time: 789 ms (6.40%), Space: 22 MB (16.48%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,27 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        visit=set()
-        Row,Col=len(grid),len(grid[0])
+        Row=len(grid)
+        Col=len(grid[0])
+        visitSet=set()
         islands=0
-        
-        
-        def bfs(r,c):
+        def bfs(i,j):
             q=[]
-            visit.add((r,c))
-            q.append((r,c))
+            visitSet.add((i,j))
+            q.append((i,j))
             while q:
-                row,col=q.pop(0)
-                directions=[[1,0],[-1,0],[0,1],[0,-1]]
-                for dr,dc  in directions:
-                    r,c=row+dr,col+dc
-                    if ( r in range(Row) and c in range(Col) and (r,c) not in visit and grid[r][c] =='1'):
-                        q.append((r,c))
-                        visit.add((r,c))
+                r,c=q.pop(0)
+                directions=[[-1,0],[1,0],[0,1],[0,-1]]
+                for dr,dc in directions:
+                    row,col=r+dr,c+dc
+                    if (row in range(Row) and col in range(Col) and (row,col) not in visitSet and grid[row][col]=='1'):
+                        visitSet.add((row,col))
+                        q.append((row,col))
         for i in range(Row):
             for j in range(Col):
-                if grid[i][j]=='1' and (i,j) not in visit: 
+                if (grid[i][j]=='1' and (i,j) not in visitSet):
                     bfs(i,j)
                     islands+=1
         return islands
+        
